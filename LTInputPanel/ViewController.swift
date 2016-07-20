071e9e50284a68b3cd9728e307343c825b5d2055
ltebean <yucong1118@gmail.com>
2016-07-20 15:39:34 +0800

    Initial Commit

---
+++ LTInputPanel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LTInputPanel
+//
+//  Created by leo on 16/7/20.
+//  Copyright © 2016年 io.ltebean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
